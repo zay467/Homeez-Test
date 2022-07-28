8b9ce9803da7a575c00995720efb891248fcdb40
Zay Min Maw <59911091+zay467@users.noreply.github.com>
2022-07-28 17:50:28 +0630

    feat: applied floorplan

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { Box } from "@mui/material";
+import { Layer, Stage } from "react-konva";
+import { Html } from "react-konva-utils";
+import Floorplan from "./components/Floorplan";
+import floorplan from "./test-resources/floorplan-sample.json";
+
 function App() {
-  return <div></div>;
+  return (
+    <Stage width={window.innerWidth} height={window.innerHeight}>
+      <Layer>
+        <Html
+          divProps={{
+            style: {
+              width: "100%",
+              height: "100%",
+              display: "flex",
+              alignItems: "center",
+              justifyContent: "center",
+              // position: "absolute",
+              // backgroundColor: "red",
+              zIndex: -1,
+            },
+          }}
+        >
+          <Box width="700px">
+            <Floorplan floorplan={floorplan.layout} />
+          </Box>
+        </Html>
+      </Layer>
+    </Stage>
+  );
 }
 
 export default App;
